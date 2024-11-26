2abdb296f022d7412fe28d0910081cc97a8fb718
Takameru <ivanishkosavely@gmail.com>
2024-11-26 22:20:11 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('lang-ru').addEventListener('click', function() {
+    // Логика переключения на русский язык
+});
+
+document.getElementById('lang-be').addEventListener('click', function() {
+    // Логика переключения на белорусский язык
+});
